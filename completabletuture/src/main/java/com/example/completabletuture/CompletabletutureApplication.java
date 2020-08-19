0b4b8d674d024c3571e1101c798302e2054a781a
dengchangshi <dengcs1@hua-cloud.com.cn>
2020-08-19 10:05:04 +0800

    第一次提交

---
+++ completabletuture/src/main/java/com/example/completabletuture/CompletabletutureApplication.java
@@ -0,0 +1,13 @@
+package com.example.completabletuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletabletutureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompletabletutureApplication.class, args);
+    }
+
+}
---
+++ completabletuture/src/test/java/com/example/completabletuture/CompletabletutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.completabletuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompletabletutureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
